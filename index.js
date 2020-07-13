2d5105359c360dc61278fa5634cdfdbbd5695fca
sheelukushwaha <38864432+sheelukushwaha@users.noreply.github.com>
2020-07-13 23:50:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const csv = require("csvtojson");
 const matchesPlayedPerYear = require("./ipl/matchesPlayedPerYear");
+const mostWins = require("./ipl/mostWins");
 const matchesWonByEachTeam = require("./ipl/matchesWonByEachTeam");
 const extraRunsConcededByEachTeamIn2016 = require("./ipl/extraRunsConcededByEachTeamIn2016");
 const tenEconomicalBowlersOf2015 = require("./ipl/tenEconomicalBowlersOf2015");
+const tossWinningTeamPerYear = require("./ipl/tossWinningTeamPerYear");
 
 const MATCHES_FILE_PATH = "./csv_data/matches.csv";
 const DELIVERIES_FILE_PATH = "./csv_data/deliveries.csv";
@@ -15,7 +17,9 @@ function main() {
     .then(matches => {   
         let result={};  
       result.matchesPlayedPerYear = matchesPlayedPerYear(matches);
+      result.mostWins = mostWins(matches);
       result.matchesWonByEachTeam = matchesWonByEachTeam(matches);
+      result.tossWinningTeamPerYear = tossWinningTeamPerYear(matches);
       
       csv()
         .fromFile(DELIVERIES_FILE_PATH)
@@ -32,9 +36,11 @@ function main() {
 function saveMatchesPlayedPerYear(result) {
   const jsonData = {
     matchesPlayedPerYear: result.matchesPlayedPerYear,
+    mostWins: result.mostWins,
     matchesWonByEachTeam: result.matchesWonByEachTeam,
     extraRunsConcededByEachTeamIn2016: result.extraRunsConcededByEachTeamIn2016,
-    tenEconomicalBowlersOf2015: result.tenEconomicalBowlersOf2015
+    tenEconomicalBowlersOf2015: result.tenEconomicalBowlersOf2015,
+    tossWinningTeamPerYear: result.tossWinningTeamPerYear
   };
   const jsonString = JSON.stringify(jsonData);
   fs.writeFile(JSON_OUTPUT_FILE_PATH, jsonString, "utf8", err => {
